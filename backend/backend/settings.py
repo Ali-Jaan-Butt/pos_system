c0f2c2b2b48d034dd89a14a978d631730135535a
Ali Jaan <aliwsservices@gmail.com>
2025-08-30 14:35:10 +0500

    cors error fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "backend_app"
+    "backend_app",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "backend.urls"
 
